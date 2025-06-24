e734888735545d4e5af5127494ee7410c37a66a2
Abhisek Sikdar <abhiseksikdar40@gmail.com>
2025-06-25 00:05:09 +0530

    fix: fixed cors.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173", "https://crm-frontend-seven-puce.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") {

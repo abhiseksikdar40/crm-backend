0e61cadf0dace0a1ce5d386a5266e6a15a03deb4
Abhisek Sikdar <abhiseksikdar40@gmail.com>
2025-06-24 11:55:27 +0530

    fix: fixed code.

--- models/leads.model.js
+++ models/leads.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const LeadsSchema = new mongoose.Schema({
     fullname: { type: String, required: true},
     leadsource: { type: String, enum: ["Website", "Referral", "Cold Call"], required: true},
-    saleseagent: { type: mongoose.Schema.Types.ObjectId, ref: "SalesAgent", required: true},
+    salesagent: { type: mongoose.Schema.Types.ObjectId, ref: "SalesAgent", required: true},
     leadstatus: { type: String, enum: ["New", "Contacted", "Qualified", "Proposal Sent", "Closed-Won", "Closed-Lost"], default: "New"},
     priority: { type: String, enum: ["High", "Medium", "Low"], default: 'Medium', required: true},
     timetoclose: { type: Number, required: true, min: 1},
